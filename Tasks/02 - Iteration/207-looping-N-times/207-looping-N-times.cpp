e72d7f6caec30f4b6e2a39a7a0eaeaa3f436c80e
Dennis-Stefan Toader <146251437+dennisnpc@users.noreply.github.com>
2023-10-05 10:11:04 +0100

    2023/10/5

--- Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
+++ Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
@@ -12,14 +12,35 @@ int main()
         //Perform code tasks
         printf("i=%d\n", i);
     }
+    printf("\n");
 
     // TASK A - count from 10 down to 0 in steps of -1
+    for (i = 10; i >= 0; i--)
+    {
+        printf("i=%d\n", i);
+    }
+    printf("\n");
 
     // TASK B - count from 10 down to -10 in steps of -2
+    for (i = 10; i >= -10; i = i - 2)
+    {
+        printf("i=%d\n", i);
+    }
+    printf("\n");
 
     // TASK C - count from 10 to 200 in steps of 10
+    for (i = 10; i <= 200; i += 10)
+    {
+        printf("i=%d\n", i);
+    }
+    printf("\n");
 
     // TASK D - count from -10 down to 20 in steps of 5
+    for (i = -10; i <= 20; i += 5)
+    {
+        printf("i=%d\n", i);
+    }
+    printf("\n");
 
     // TASK E - can you predict the largest value of i that is displayed?
     for (i = 0; i < 12; i += 3)
