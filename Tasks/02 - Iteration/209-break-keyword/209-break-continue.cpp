2de7b30ff74400e27c54d1fa6e829b21b12c8cfc
Dennis-Stefan Toader <146251437+dennisnpc@users.noreply.github.com>
2023-10-07 19:12:17 +0100

    .

--- Tasks/02 - Iteration/209-break-keyword/209-break-continue.cpp
+++ Tasks/02 - Iteration/209-break-keyword/209-break-continue.cpp
@@ -2,15 +2,22 @@
 
 int main()
 {
-    for (int i = 0; i <= 10; i++)   // potentially loop from 1 to 10  
+    int exitEarly = 0;
+    for (int i = 0; 1 == 1; i++)   // potentially loop from 1 to 10  
     {
         char c = getchar(); //Read character
         char _ = getchar(); //Read and discard newline (nice little hack :)
-        if (c == 'q')          
+        if (c == 'q' || c == 'Q')
         {
             printf("\tUser stopped the loop early at %d\n", i);
+            exitEarly = 1;
             break;                  // break out of the for loop
         }
+
+        if (exitEarly == 1) {
+            printf("Loop has exited early\n");
+        }
+       
         printf("Loop counter = %d\n", i);
     }
     puts("Done");
